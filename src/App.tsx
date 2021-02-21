8e8751fca067df124d44a2c9900841cd2cabc7db
Dirron <dillonoudomsouk@gmail.com>
2021-02-21 15:36:49 -0500

    create basic timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = (): JSX.Element => {
+  const [count, setCount] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    if (isActive) {
+      const timerId = setTimeout(() => setCount((prev) => prev + 1), 1000);
+
+      return () => clearTimeout(timerId);
+    }
+  }, [isActive, count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <span className="timerLine">
+        <div className="count">{count}</div>
+        <div className="seconds">s</div>
+      </span>
+      <button
+        className="timerButton"
+        onClick={() => setIsActive((prev) => !prev)}
+      >
+        {isActive ? "Stop" : "Start"}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
